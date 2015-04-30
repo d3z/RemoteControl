263c4e2eed4c4d8d9517fdc6bfafaf04f0e88a16
Gareth Fleming <gareth.fleming@instil.co>
2015-04-30 14:48:15 +0100

    initial commit of index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+(function() {
+
+    'use strict';
+
+    var app = require('express')();
+    var http = require('http').Server(app);
+    var io = require('socket.io')(http);
+
+    var left = io.of('/left');
+    var right = io.of('/right');
+
+    app.get('/left', function(req, res) {
+        left.emit('move', {'direction':'left'});
+        res.status(200, 'OK');
+    });
+
+    app.get('/right', function(req, res) {
+        right.emit('move', {'direction':'right'});
+        res.status(200, 'OK');
+    });
+
+    app.get('/forward', function(req.res) {
+        left.emit('move', {'direction':'forward'});
+        right.emit('move', {'direction':'forward'});
+        res.status(200, 'OK');
+    });
+
+    var port = process.env.RC_PORT || 5000;
+    app.listen(port, function() {
+        console.log('RemoteControl is listening on port', port);
+    });
+
+
+})();
